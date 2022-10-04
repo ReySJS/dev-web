5aae3f520bdc6fac350fda0276345dce872dafdd
Rey <rey.filareto@outlook.com>
2022-10-04 02:38:40 -0300

    Update

--- 2022/nv2/29-09/scripts/index.js
+++ 2022/nv2/29-09/scripts/index.js
@@ -1,14 +1,19 @@
+// Variável de tipo 'objeto', caracterizada pela presença de chaves ({})
 let users = {
+  // chave(key) : valor(value)
   "rey@gmail.com": "123456789",
 };
 
-let loggedUser = {};
-
+// Função para verificar os dados de usuário e efetuar o login
 function login() {
+  // Captura do email e senha através dos inputs
   const email = document.getElementById("input-email").value;
   const password = document.getElementById("input-password").value;
 
+  // Essa parte do código verifica se a variável 'users' possui alguma chave(key)
+  // igual ao email recebido no input e compara o seu valor(value) com a senha recebida no input
   if (users[email] == password) {
+    // método usado para redirecionar o usuário entre arquivos html
     window.location.href = "./dashboard.html";
     return;
   }
